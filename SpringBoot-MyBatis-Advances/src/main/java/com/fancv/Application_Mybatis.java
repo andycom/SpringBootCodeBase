5e4248e8369b8e22c911756a0449aa266a2dfbbf
Andy <winglive@163.com>
2021-04-19 22:47:33 +0800

    springboot mybatis advance demo  : 计划

--- SpringBoot-MyBatis-Advances/src/main/java/com/fancv/Application_Mybatis.java
+++ SpringBoot-MyBatis-Advances/src/main/java/com/fancv/Application_Mybatis.java
@@ -4,6 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+//TODO
+
+/**
+ * 1.mybitais 拦截器demo
+ * 2.mybatis aop 多数据源
+ * 3.数据库连接池
+ * 4.分库分表
+ */
 @SpringBootApplication
 @EnableSwagger2
 public class Application_Mybatis {
