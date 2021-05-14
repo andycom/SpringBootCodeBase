5be545e158596b4195ae25cfb040a97a041b5a02
andy <winglive@163.com>
2021-05-14 18:06:18 +0800

    SpringBoot zookeeper

---
+++ springBoot-zookeeper/src/main/java/com/fancv/SpringBootZooKeepers.java
@@ -0,0 +1,12 @@
+package com.fancv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootZooKeepers {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootZooKeepers.class, args);
+    }
+}
